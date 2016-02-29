2b962ba60d3d97484676dd9ac2ab5ca42403b80c
ezio84 <brabus84@gmail.com>
2016-02-29 22:44:29 +0100

    Add back Torch action [1/2]

--- core/java/com/android/internal/util/slim/Action.java
+++ core/java/com/android/internal/util/slim/Action.java
@@ -23,6 +23,9 @@ import android.content.ActivityNotFoundException;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CameraCharacteristics;
+import android.hardware.camera2.CameraAccessException;
 import android.hardware.input.InputManager;
 import android.media.AudioManager;
 import android.media.session.MediaSessionLegacyHelper;
@@ -54,6 +57,8 @@ public class Action {
 
     private static final int MSG_INJECT_KEY_DOWN = 1066;
     private static final int MSG_INJECT_KEY_UP = 1067;
+
+    private static boolean sTorchEnabled = false;
 
     private static final int STATE_ENABLE_FOR_ALL = 1;
     private static final int STATE_USER_CONFIGURABLE = 2;
@@ -148,6 +153,22 @@ public class Action {
                 try {
                     barService.startAssist(new Bundle());
                 } catch (RemoteException e) {}
+            } else if (action.equals(ActionConstants.ACTION_TORCH)) {
+                try {
+                    CameraManager cameraManager = (CameraManager)
+                            context.getSystemService(Context.CAMERA_SERVICE);
+                    for (final String cameraId : cameraManager.getCameraIdList()) {
+                        CameraCharacteristics characteristics =
+                            cameraManager.getCameraCharacteristics(cameraId);
+                        int orient = characteristics.get(CameraCharacteristics.LENS_FACING);
+                        if (orient == CameraCharacteristics.LENS_FACING_BACK) {
+                            cameraManager.setTorchMode(cameraId, !sTorchEnabled);
+                            sTorchEnabled = !sTorchEnabled;
+                        }
+                    }
+                } catch (CameraAccessException e) {
+                }
+                return;
             } else if (action.equals(ActionConstants.ACTION_SMART_PULLDOWN)) {
                 if (isKeyguardShowing && isKeyguardSecure) {
                     return;
